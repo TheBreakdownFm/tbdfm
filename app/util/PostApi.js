c2e9aec95d31bcb784b90771e61cee7036c3d739
TheBreakdownFm <email@thebreakdown.fm>
2015-07-28 10:08:08 -0400

    maybe forgot to add some shit

---
+++ app/util/PostApi.js
@@ -0,0 +1,24 @@
+import alt from '../alt'
+import actions from '../actions/AppActions'
+
+let PostApi = {
+  getPosts(offset){
+    if(offset){
+    
+    } else {
+    
+    }
+  }
+  getSinglePost(id){
+  }
+
+  savePost(post){
+  }
+
+  updatePost(post){
+  }
+
+
+}
+
+export default PostApi;
